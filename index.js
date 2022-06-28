ed5538c4322865ab8e7274f2941efd9b8a573115
Gabriel G <gabrieldeoliveira05@gmail.com>
2022-06-28 00:36:11 -0300

    checkpoin3

--- index.js
+++ index.js
@@ -1,31 +1,62 @@
+/* 1- Crie uma função construtora que tenha como atributos: nome (string), quantidade de faltas (number) e notas (array de números). */
+/* 2- Na função construtora crie o método calcularMedia que retorna a média de suas notas. Também terá um método chamado faltas, 
+que simplesmente aumenta o número de faltas em 1. Crie alguns alunos para testar a sua função construtora.  */
 
 function Alunos(nome, qtdeFaltas, notas){
     this.nome = nome;
     this.qtdeFaltas = qtdeFaltas;
     this.notas = notas;    
     
-    this.mediaNotas = function(notas) {       
+    this.media = function calcularMedia (notas) {       
         let sum = 0;
         for(let i = 0; i < this.notas.length; i++) {
             sum += this.notas[i]; // [i] a posição do array é a msma da do contador.
+            
         }
         return (sum / this.notas.length);
     }
 }
 
+const Frodo = new Alunos('Frodo', 1,[5,9,5]);
+const Sauron = new Alunos('Sauron', 0,[10,9,9]);
+const Gandalf = new Alunos('Gandalf', 0,[10,10,10]);
+
 let Curso = {
-   nome: String,
-   aprovacao: Number,
-   faltasMax: Number,
-   listaAlunos: Array,
+   nome: 'Bebidas e Poçoes Mágicas',
+   aprovacao: 7,
+   faltasMax: 5,
+   listaAlunos: [Frodo,Sauron,Gandalf],
+
+    addAluno(nome, qtdeFaltas, notas) {
+        let novoAluno = new Alunos(nome,qtdeFaltas,notas);
+        this.listaAlunos.push(novoAluno);
+   }
 }
 
-const aluno1 = new Alunos('Harry Potter', 1,[5,5,5]);
-const aluno2 = new Alunos('Hermione Granger', 0,[10,9,9]);
-const aluno3 = new Alunos('Ronald Weasley', 3,[5,9,3]);
-
-console.log(aluno1.mediaNotas());
-console.log(aluno2.mediaNotas());
-console.log(aluno3.notas);
+Curso.addAluno('Legolas', 5, [5,9,9]);
+console.log(Frodo.media());
+console.log(Sauron.notas);
+console.log(Curso.listaAlunos);
 
 
+
+// resultadoDosAlunos() {
+//     for (let i = 0; i < this.listaAlunos.length; i++) {
+//       const alunoCurso = this.listaAlunos[i];
+//       let media = alunoCurso.media;
+//       let media10 = this.notaAprovacao + this.notaAprovacao * 0.1;
+//       let faltas = alunoCurso.qtfaltas;
+//       let aprovado = [
+//         (media >= this.notaAprovacao && faltas < this.faltasMaximas) ||
+//         (faltas >= this.faltasMaximas && media >= media10)
+//           ? true
+//           : false,
+//       ];
+//       let resultado = []
+//       resultado.push(aprovado)
+      
+//       return resultado
+      
+//     }
+//   },
+// };
